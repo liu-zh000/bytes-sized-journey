0cb6d1c4b0aa7ec6164a97beefd4cbc60bdbf205
liu-zh000 <2977724916@qq.com>
2025-07-20 22:29:49 +0800

    vvvvvv

---
+++ daima_note.md
@@ -0,0 +1,56 @@
+# 代码随想录
+
+## 一、数组
+
+### 二分法
+在循环中始终坚持根据查找区间的定义来做边界处理。
+
+
+1.次方运算
+
+```cpp
+{
+    #include <cmath>
+    double result = pow(10, i);  // 返回 double 
+    int intResult = static\_cast\<int>(pow(10, i));  // 强制转换
+}
+```
+2.字符数字转换
+![alt text](image.png)
+```cpp
+
+    string num1;
+    `num1[i] - '0'`
+```
+3.前缀和 + 二分查找
+```cpp
+{
+    class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int ans = INT_MAX;
+        vector<int> sums(n + 1, 0); 
+        // 为了方便计算，令 size = n + 1 
+        // sums[0] = 0 意味着前 0 个元素的前缀和为 0
+        // sums[1] = A[0] 前 1 个元素的前缀和为 A[0]
+        // 以此类推
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int target = s + sums[i - 1];
+            auto bound = lower_bound(sums.begin(), sums.end(), target);
+            if (bound != sums.end()) {
+                ans = min(ans, static_cast<int>((bound - sums.begin()) - (i - 1)));
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
+
+}
+```
--- leetcode.cpp
+++ leetcode.cpp
@@ -1,54 +1,120 @@
 
-#include <string>
-#include <iostream>
-#include <vector>
-using namespace std;
-class Solution {
-public:
-    string multiply(string num1, string num2) {
-        int a = num1.size();//123
-        int b = num2.size();
-        int cheng10 = 1;
-        int all1 = 0 ;
-        int cheng = 0;
-        int numn1i = 0 ; int  numn2i = 0;
-        vector<int> res ;
-        //string res = "";
-        int m = 0 ;int jinwei = 0;
-        for(int i = b-1 ;i>=0 ; i--)//456
-        {
+ #include <string>
+ #include <iostream>
+ #include <vector>
+ using namespace std;
+// class Solution {
+// public:
+//     string multiply(string num1, string num2) {
+//         int a = num1.size();//123
+//         int b = num2.size();
+//         int cheng10 = 1;
+//         int all1 = 0 ;
+//         int cheng = 0;
+//         int numn1i = 0 ; int  numn2i = 0;
+//         vector<int> res ;
+//         //string res = "";
+//         int m = 0 ;int jinwei = 0;
+//         for(int i = b-1 ;i>=0 ; i--)//456
+//         {
             
-            for(int j = a-1 ;j >=0 ; j--)
-            {
-                cheng = (num2[i] - '0') * (num1[j] - '0');
-                cheng = cheng * cheng10;
-                cheng10 = 10 * cheng10;
-                all1 = all1 + cheng;
-                //res[m] = cheng%10 + jinwei;
-                //jinwei = cheng/10;  
-                //m++;
-            }
-            res.push_back(all1);
-            all1 = 0;
-            cheng10 = 1;
-        }
+//             for(int j = a-1 ;j >=0 ; j--)
+//             {
+//                 cheng = (num2[i] - '0') * (num1[j] - '0');
+//                 cheng = cheng * cheng10;
+//                 cheng10 = 10 * cheng10;
+//                 all1 = all1 + cheng;
+//                 //res[m] = cheng%10 + jinwei;
+//                 //jinwei = cheng/10;  
+//                 //m++;
+//             }
+//             res.push_back(all1);
+//             all1 = 0;
+//             cheng10 = 1;
+//         }
 
-        int sum = 0 ;
-        for(int l = 0; l <= res.size() -1  ; l++ )
-        {
-            sum  = sum + res[l]*cheng10;
-             cheng10 = 10 * cheng10;
-        }
+//         int sum = 0 ;
+//         for(int l = 0; l <= res.size() -1  ; l++ )
+//         {
+//             sum  = sum + res[l]*cheng10;
+//              cheng10 = 10 * cheng10;
+//         }
 
         
-        return to_string(sum);
-    }
+//         return to_string(sum);
+//     }
+// };
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int largem = nums[0]; int ptr = 0;int sum =0;
+        int l = 1;
+        for (int i = 0 ; i < nums.size() ; i ++ )
+        {
+            if(largem < nums[i])
+            {
+                largem = nums[i];
+                ptr = i ;
+            }
+        }
+
+        if (largem >= target)
+        {
+            return 1;
+        }
+        else
+        {
+            sum = largem;
+            int ptr2 = ptr ;
+            while(sum < target)
+            {
+
+                if(ptr2 - 1 < 0  && ptr + 1 >= nums.size())
+                {
+                    return 0;
+                }
+
+                if(ptr + 1 < nums.size() || ptr2 - 1 >= 0)
+                {
+                    if(nums[ptr + 1] >= nums[ptr2 - 1])
+                    {
+                        sum += nums[ptr + 1 ];
+                        ptr ++ ;
+                        l++;
+                    }
+                    else
+                    {
+                        sum += nums[ptr2 - 1 ];
+                        ptr2 --;
+                        l++;
+                    }
+                }
+                else if(ptr + 1 >= nums.size() && ptr2 - 1 >=  0)
+                {
+                    sum += nums[ptr2 - 1 ];
+                        ptr2 --;
+                        l++;
+                }
+                else if(ptr2 - 1 <  0)
+                {
+                    sum += nums[ptr + 1 ];
+                        ptr ++ ;
+                        l++;
+                }
+
+            }
+            return  l; 
+
+        }
+     }
 };
 
 int main()
 {
     Solution a;
-    string result = a.multiply("123","456");
+    vector<int> ab ={1,1,1,1,1,1,1,1};
+    int result = a.minSubArrayLen(11 , ab);
     std::cout << "½á¹û"<<result << std::endl;
 }
 
